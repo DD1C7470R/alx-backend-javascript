8e87fb2d41a82fc1ad7d98d73425ab937e1613d9
DD1C7470R <gracevalley101@gmail.com>
2024-02-27 22:35:08 +0000

    Add task 1 update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,12 @@
-const { spawn } = require('node:child_process');
-
-const cmd = spawn('echo', ['Welcome to Holberton School, what is your name?']);
+process.stdout.write('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
-cmd.stdout.on('data', (data) => {
-  process.stdout.write(data);
-  process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk != null) {
-      process.stdout.write(`Your name is: ${chunk}`);
-    }
-  });
-  process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing \n');
-  });
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk != null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing \n');
 });
