ead3e2eca46d75507c6db4ea260de42733825c49
DD1C7470R <gracevalley101@gmail.com>
2024-02-28 18:51:12 +0000

    Update to use strict check

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,12 +3,12 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('test calculator', () => {
-  it('roud and a and b and return the sum', () => {
+  it('round and a and b and return the sum', () => {
     const result = calculateNumber(3.345, 6.9);
-    assert.equal(result, 10);
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(result, 10);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
