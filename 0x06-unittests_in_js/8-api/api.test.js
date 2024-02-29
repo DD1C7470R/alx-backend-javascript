63fc96819ae88f300569ad4d0797e542314b54f1
DD1C7470R <gracevalley101@gmail.com>
2024-02-29 19:04:18 +0000

    Fix lints

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,18 @@
 const chai = require('chai');
 const request = require('request');
 
-
-describe('Full HTTP server using Express', () => {
+describe('full HTTP server using Express', () => {
   describe('/ endpoint', () => {
-    it('Returns the right content', (done) =>  {
-		const options = {
-			url: "http://localhost:7865/",
-			method: "GET"
-		}
+    it('returns the right content', () => new Promise((done) => {
+      const options = {
+        url: 'http://localhost:7865/',
+        method: 'GET',
+      };
       request(options, (err, response, body) => {
-		chai.expect(body).to.have.string('Welcome to the payment system');
-		chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.have.string('Welcome to the payment system');
+        chai.expect(response.statusCode).to.equal(200);
 	  });
 	  done();
-	});
+    }));
   });
-});
+);
