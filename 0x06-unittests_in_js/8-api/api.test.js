93d069e44ebe38edd034631b815a6acdca225584
DD1C7470R <gracevalley101@gmail.com>
2024-02-29 19:05:55 +0000

    Fix lints

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 describe('full HTTP server using Express', () => {
   describe('/ endpoint', () => {
-    it('returns the right content', () => new Promise((done) => {
+    it('returns the right content', (done) => {
       const options = {
         url: 'http://localhost:7865/',
         method: 'GET',
