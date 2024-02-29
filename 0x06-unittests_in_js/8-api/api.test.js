9a270fd3f2d82b688200f12ea01d1096c60efedf
DD1C7470R <gracevalley101@gmail.com>
2024-02-29 19:12:30 +0000

    Hot fix

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,21 +1,28 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-const app = require('./api');
-
-chai.use(chaiHttp);
-chai.should();
-
-describe('Full HTTP server using Express', () => {
-  describe('/ endpoint', () => {
-    it('Returns the right content', (done) => {
-      chai.request(app)
-        .get('/')
-        .end((error, response) => {
-          chai.expect(response.text).to.equal('Welcome to the payment system');
-          chai.expect(response.statusCode).to.equal(200);
-          done();
-        });
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+    it("check correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
+    });
 });
